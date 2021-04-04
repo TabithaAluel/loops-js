91dd4185a8ae444f1f232ffe70bcd0ac042673e3
TabithaAluel <alueltabitha@gmail.com>
2021-04-04 23:14:26 +0300

    Js Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const names=["Tabitha","Isaac","Jacob","Rebecca"];for(const n of names){
+    console.log(n);
+}
+const symbols={
+    Tabby: "Tabitha",
+    Izo: "Isaac",
+    Jay: "Jacob",
+    Becky: "Rebecca",
+}
+for(const n in symbols){
+    console.log(n);
+}
